b262973a2cd1facd1f7564337e33a306416b5f15
NikoOstrouhov <ostrouhov1494@gmail.com>
2024-11-05 18:18:21 +0300

    second commit

--- PP lab2/PP lab2.cpp
+++ PP lab2/PP lab2.cpp
@@ -3,7 +3,7 @@
 #include <memory>
 #include "ThreadPool.h"
 
-const int COUNT_OF_NUMBERS = 1000;
+const int COUNT_OF_NUMBERS = 10;
 
 void job(int* arr, int n, int start, int end);
 int* initArray(int n);
